e8d1b0cf18087ac53528624edc97c39f6d3a9108
daniel <DanRhysMorris@gmail.com>
2023-04-11 14:03:12 +0100

    Add some assertions to Frame::Split and Append

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -89,11 +89,12 @@ namespace Strawberry::Codec
 						0, 0,
 						mFrame->nb_samples, mFrame->ch_layout.nb_channels,
 						static_cast<AVSampleFormat>(mFrame->format));
-		av_samples_copy(newFrame->data, other.mFrame->data,
+		av_samples_copy(newFrame->data, other->data,
 						mFrame->nb_samples, 0,
-						other.mFrame->nb_samples, other.mFrame->ch_layout.nb_channels,
-						static_cast<AVSampleFormat>(other.mFrame->format));
+						other->nb_samples, other->ch_layout.nb_channels,
+						static_cast<AVSampleFormat>(other->format));
 
+		Core::Assert(newFrame->nb_samples == mFrame->nb_samples + other->nb_samples);
 		std::destroy_at(this);
 		mFrame = newFrame;
 	}
@@ -104,16 +105,15 @@ namespace Strawberry::Codec
 	{
 		pos = std::clamp<size_t>(pos, 0, mFrame->nb_samples);
 
-		int error = 0;
-
 		AVFrame* frames[2] = { av_frame_alloc(), av_frame_alloc() };
-
 		frames[0]->format		= mFrame->format;
 		frames[1]->format		= mFrame->format;
 		frames[0]->ch_layout	= mFrame->ch_layout;
 		frames[1]->ch_layout	= mFrame->ch_layout;
 		frames[0]->nb_samples	= pos;
 		frames[1]->nb_samples	= mFrame->nb_samples - pos;
+
+		int error = 0;
 		error = av_frame_get_buffer(frames[0], 0);
 		Core::Assert(error == 0);
 		error = av_frame_make_writable(frames[0]);
@@ -132,6 +132,7 @@ namespace Strawberry::Codec
 						mFrame->nb_samples - pos, mFrame->ch_layout.nb_channels,
 						static_cast<AVSampleFormat>(mFrame->format));
 
+		Core::Assert(frames[0]->nb_samples + frames[1]->nb_samples == mFrame->nb_samples);
 		return { Frame(frames[0]), Frame(frames[1]) };
 	}
 
