76c5484a59d5be4f55e83d1e1ed1213751165f2a
StrawberryDan <DanRhysMorris@gmail.com>
2023-07-02 15:29:18 +0100

    Update AudioMixer method to dynamically handle channels
    This commit includes an update to the AudioMixer class methods, moving from a static to a dynamic channel handling. Instead of pre-defining the number of channels, they are now dynamically figured out during runtime in the `SendFrame` and `RecvFrame` methods. This makes the class more adaptable and robust to varied input sizes. This change also includes the inclusion of 'fmt/format.h' library to support dynamic channel handling.

--- src/AudioMixer.cpp
+++ src/AudioMixer.cpp
@@ -2,6 +2,8 @@
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
 #include "Codec/AudioMixer.hpp"
+// Lib Format
+#include "fmt/format.h"
 
 
 namespace Strawberry::Codec
@@ -9,25 +11,39 @@ namespace Strawberry::Codec
 	AudioMixer::AudioMixer(unsigned int channelCount)
 		: FilterGraph(MediaType::Audio)
 	{
-		AddOutput("");
-		for (int i = 0; i < channelCount; i++)
-		{
-			AddInput("");
-		}
-
-		Configure();
-		Start();
+		AddFilter("amix", "mixer", fmt::format("inputs={}", channelCount)).Unwrap();
 	}
 
 
 	void AudioMixer::SendFrame(unsigned int channel, Frame frame)
 	{
+		Core::Assert(channel <= GetInputCount());
+		if (channel == GetInputCount())
+		{
+			Stop();
+			auto newInput = AddInput(fmt::format("sample_fmt={}:channels={}:sample_rate={}:channel_layout={}:", frame->format, frame->ch_layout.nb_channels, frame->sample_rate, frame->channel_layout)).Unwrap();
+			auto formatter = AddFilter("aformat", fmt::format("formatter-{}", GetInputCount() - 1), "channel_layouts=stereo").Unwrap();
+			auto mixer = GetFilter("mixer").Unwrap();
+			newInput->Link(*formatter, 0, 0);
+			formatter->Link(*mixer, 0, GetInputCount() - 1);
+		}
+
 		FilterGraph::SendFrame(channel, frame);
 	}
 
 
 	Core::Option<Frame> AudioMixer::RecvFrame()
 	{
+		if (GetOutputCount() == 0)
+		{
+			Stop();
+			auto output = AddOutput("").Unwrap();
+			auto mixer = GetFilter("mixer").Unwrap();
+			mixer->Link(*output, 0, 0);
+			Configure();
+		}
+
+		Start();
 		return FilterGraph::RecvFrame(0);
 	}
 }
