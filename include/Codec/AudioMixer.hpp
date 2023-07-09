cd06abfe24e83dc2b67d250ec196a498d2c89188
StrawberryDan <DanRhysMorris@gmail.com>
2023-07-09 20:37:36 +0100

    Implement rudimentary AudioMixer implementation. This will require so refactoring but right now it is an excellent proof of concept.

--- include/Codec/AudioMixer.hpp
+++ include/Codec/AudioMixer.hpp
@@ -5,18 +5,27 @@
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
 /// Codec
+#include "AudioFrameResizer.hpp"
+#include "Codec/Resampler.hpp"
 #include "FilterGraph.hpp"
 #include "Frame.hpp"
 /// Strawberry Libraries
 #include "Strawberry/Core/Option.hpp"
 
 
-
 namespace Strawberry::Codec
 {
 	class AudioMixer
-			: private FilterGraph
 	{
+	public:
+		AudioMixer(size_t trackCount);
 
+
+		void Send(size_t trackIndex, const Frame& frame);
+		Core::Option<Frame> ReceiveFrame();
+
+	private:
+		size_t mTrackCount;
+		std::vector<Resampler> mResamplers;
 	};
 }
--- src/AudioMixer.cpp
+++ src/AudioMixer.cpp
@@ -8,7 +8,66 @@
 #include "fmt/format.h"
 
 
+constexpr size_t FRAME_SIZE = 48000 / 32;
+constexpr int    FORMAT = AV_SAMPLE_FMT_DBL;
+
+;
 namespace Strawberry::Codec
 {
+	AudioMixer::AudioMixer(size_t trackCount)
+		: mTrackCount(trackCount)
+		, mResamplers()
+	{
+		AudioFrameFormat format(48000, FORMAT, AV_CHANNEL_LAYOUT_STEREO);
 
+		for (int i = 0; i < trackCount; i++)
+		{
+			mResamplers.emplace_back(format);
+			mResamplers.back().SetOutputFrameSize(FRAME_SIZE);
+		}
+	}
+
+
+	void AudioMixer::Send(size_t trackIndex, const Frame& frame)
+	{
+		Core::Assert(trackIndex >= 0 && trackIndex < mTrackCount);
+		mResamplers[trackIndex].SendFrame(frame);
+	}
+
+
+	Core::Option<Frame> AudioMixer::ReceiveFrame()
+	{
+		AudioFrameFormat format(48000, FORMAT, AV_CHANNEL_LAYOUT_STEREO);
+
+		std::vector<Frame> inputs;
+		inputs.reserve(mTrackCount);
+		int tracksReadFrom = 0;
+		for (int i = 0; i < mTrackCount; i++)
+		{
+			auto frame = mResamplers[i].ReadFrame();
+			if (frame) tracksReadFrom += 1;
+			inputs.emplace_back(std::move(frame.UnwrapOr(Frame::Silence(format, FRAME_SIZE))));
+		}
+
+		if (tracksReadFrom == 0) return {};
+
+		Frame frame = Frame::Silence(format, FRAME_SIZE);
+		for (int inputIndex = 0; inputIndex < inputs.size(); inputIndex++)
+		{
+			for (int sampleIndex = 0; sampleIndex < FRAME_SIZE * 2; sampleIndex++)
+			{
+				for (int i = 0; i < AV_NUM_DATA_POINTERS; i++)
+				{
+					if (frame->data[i])
+					{
+						auto& input = reinterpret_cast<double*>(inputs[inputIndex]->data[i])[sampleIndex];
+						auto& output = reinterpret_cast<double*>(frame->data[i])[sampleIndex];
+						output += input / tracksReadFrom;
+					}
+				}
+			}
+		}
+
+		return frame;
+	}
 }
