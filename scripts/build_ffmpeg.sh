eaaadba6148b6785f93ddf3dd729ac233406350f
dmorris <dmorris@feralinteractive.com>
2023-08-07 14:27:18 +0100

    The cloning and building of ffmpeg now runs at configure time so that pkg-config can be used to generate the build arguments.

--- scripts/build_ffmpeg.sh
+++ scripts/build_ffmpeg.sh
@@ -3,10 +3,16 @@
 SOURCE_DIR=$1
 BINARY_DIR=$2
 
-cd ${SOURCE_DIR}
 
-if [[ ! -f configure_flag ]]; then ./configure --prefix="${BINARY_DIR}" && touch configure_flag; fi
+if [[ ! -d ${BINARY_DIR} ]]; then
+  mkdir -p ${SOURCE_DIR}
 
-if ! make -q; then make -j 4 && make install; fi
+  cd ${SOURCE_DIR}
+  git clone "https://git.ffmpeg.org/ffmpeg.git" .
+  git checkout "release/6.0"
 
+  if [[ ! -f configure_flag ]]; then ./configure --prefix="${BINARY_DIR}" && touch configure_flag; fi
+
+  if ! make -q; then make -j 4 && make install; fi
+fi
 exit 0
