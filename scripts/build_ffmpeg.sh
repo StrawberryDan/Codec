b966813c44b825b2821a9ed1cf1953875505514f
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-11 11:58:41 +0100

    Changed the build_ffmpeg.sh script to configure ffmpeg without checking configure_flag.
    This modification was necessary so that ffmpeg is configured every time with the disable-programs and disable-doc flags, for preventing the compilation of unnecessary programs and documentation material, respectively, thus saving time and resources during the build process.

--- scripts/build_ffmpeg.sh
+++ scripts/build_ffmpeg.sh
@@ -11,7 +11,7 @@ if [[ ! -d ${BINARY_DIR} ]]; then
   git clone "https://git.ffmpeg.org/ffmpeg.git" .
   git checkout "release/6.0"
 
-  if [[ ! -f configure_flag ]]; then ./configure --prefix="${BINARY_DIR}" && touch configure_flag; fi
+  ./configure --prefix="${BINARY_DIR}" --disable-programs --disable-doc
 
   if ! make -q; then make -j 4 && make install; fi
 fi
